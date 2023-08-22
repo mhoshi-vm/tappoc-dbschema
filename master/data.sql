4d02755255955bdd368f1d79ea875e9997dd4bae
machih <machih@vmware.com>
2023-08-23 01:50:32 +0900

    Add new data

--- master/data.sql
+++ master/data.sql
@@ -2,4 +2,9 @@ INSERT INTO person (id, last_name, first_name, house_id) VALUES (1, 'Hoshino', '
 INSERT INTO person (id, last_name, first_name, house_id) VALUES (2, 'Hoshino', 'woman', 1);
 INSERT INTO person (id, last_name, first_name, house_id) VALUES (3, 'Hoshino', 'son', 1);
 
+INSERT INTO person (id, last_name, first_name, house_id) VALUES (4, 'Sasaki', 'man', 2);
+INSERT INTO person (id, last_name, first_name, house_id) VALUES (5, 'Sasaki', 'woman', 2);
+INSERT INTO person (id, last_name, first_name, house_id) VALUES (6, 'Sasaki', 'son', 2);
+
 INSERT INTO house VALUES (1, 'Tokyo Shimin House');
+INSERT INTO house VALUES (2, 'Tokyo Goka House');
